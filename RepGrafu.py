1e059d14cbe12c147bdc4cb4517fb38ca9198441
ikami1 <chicken3579@o2.pl>
2016-11-20 17:17:54 +0100

    Add files via upload

--- RepGrafu.py
+++ RepGrafu.py
@@ -1,10 +1,13 @@
+#Reprezentacja grafu
 
 def funkcja():
-    graph = {}
-    for i in range(10):
-        graph[i] = [(i,i)]
+    odwiedzony = [False]                    #Zeby liczylo od 1 a nie 0
+    graph = {}                              #graf = {wierzcholek : [(wierz_konc1, waga) , (wierz_konc2, waga)], wierzcholek_nast...}
+    for i in range(1,10):
+        #graph[i] = [(i,i)]
+        odwiedzony.append(False)
 
     for node in graph:
-        print(graph[node])
+        print(graph[node], odwiedzony[node])
 
 funkcja()
