b18cfde414ba36617b78404c63cd584a5e85ae4a
ikami1 <chicken3579@o2.pl>
2016-11-20 20:03:31 +0100

    Add files via upload

--- RepGrafu.py
+++ RepGrafu.py
@@ -5,10 +5,15 @@ def funkcja():
     odwiedzony = [False]                        #Zeby liczylo od 1 a nie 0
     graph = {}                                  #graf = {wierzcholek : [(wierz_konc1, waga) , (wierz_konc2, waga)], wierzcholek_nast...}
     for i in range(1,x + 1):
-        graph[i] = [(i,i)]
+        add_node(graph, i)
         odwiedzony.append(False)
 
     for node in graph:
         print(graph[node], odwiedzony[node])
+
+def add_node(graph, node):
+    #Wstawia wierzchołek do grafu
+    if node not in graph:
+        graph[node] = []
 
 funkcja()
